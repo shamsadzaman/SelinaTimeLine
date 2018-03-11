6cd4cb1d7c1ad9c454fb932fb5cc0bef975ad00e
Shamsad Zaman <shamsad.zaman@selise.ch.bd>
2018-03-11 13:23:47 +0600

    show multiple marker based on event type

--- SelinaTimeLine/Controls/TimeLineTickBar.cs
+++ SelinaTimeLine/Controls/TimeLineTickBar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using SelinaTimeLine.Models;
@@ -11,6 +12,76 @@ namespace SelinaTimeLine.Controls
     public class TimeLineTickBar : TickBar
     {
         public static readonly DependencyProperty EventMarkersProperty = DependencyProperty.Register("EventMarkers", typeof(List<EventMarker>), typeof(TimeLineTickBar), new PropertyMetadata(default(List<EventMarker>)));
+        public static readonly DependencyProperty GoalProperty = DependencyProperty.Register("Goal", typeof(Canvas), typeof(TimeLineTickBar), new PropertyMetadata(default(Canvas), GPropertyChangedCallback));
+
+        private static void GPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            if (dependencyObject is TimeLineTickBar d)
+            {
+                GoalEventMarker = GetDrawingImageFrom(d.Goal);
+            }
+        }
+
+        public static readonly DependencyProperty FoulProperty = DependencyProperty.Register("Foul", typeof(Canvas), typeof(TimeLineTickBar), new PropertyMetadata(default(Canvas), FPropertyChangedCallback));
+
+        private static void FPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            if (dependencyObject is TimeLineTickBar d)
+            {
+                FoulEventMarker = GetDrawingImageFrom(d.Foul);
+            }
+        }
+
+        public List<EventMarker> EventMarkers
+        {
+            get => (List<EventMarker>)GetValue(EventMarkersProperty);
+            set => SetValue(EventMarkersProperty, value);
+        }
+
+        private static DrawingImage FoulEventMarker { get; set; }
+
+        private static DrawingImage GoalEventMarker { get; set; }
+
+        public Canvas Goal
+        {
+            get { return (Canvas) GetValue(GoalProperty); }
+            set { SetValue(GoalProperty, value); }
+        }
+
+        public Canvas Foul
+        {
+            get { return (Canvas) GetValue(FoulProperty); }
+            set { SetValue(FoulProperty, value); }
+        }
+
+
+        private static DrawingImage GetDrawingImageFrom(Canvas path)
+        {
+            //var path = Foul; //(FrameworkElement)Application.Current.FindResource(resourceName);
+            var vb = new VisualBrush(path);
+            var gd = new GeometryDrawing(
+                vb,
+                new Pen(Brushes.Transparent, 0),
+                new RectangleGeometry(new Rect(0, 0, path.Width, path.Height)));
+            var di = new DrawingImage(gd);
+
+            // di.Freeze();
+            return di;
+        }
+
+        //private DrawingImage GetDrawingImageFrom(string resourceName)
+        //{
+        //    var path = (FrameworkElement)Application.Current.FindResource(resourceName);
+        //    var vb = new VisualBrush(path);
+        //    var gd = new GeometryDrawing(
+        //        vb,
+        //        new Pen(Brushes.Transparent, 0),
+        //        new RectangleGeometry(new Rect(0, 0, path.Width, path.Height)));
+        //    var di = new DrawingImage(gd);
+
+        //    // di.Freeze();
+        //    return di;
+        //}
 
         protected override void OnRender(DrawingContext dc)
         {
@@ -29,7 +100,6 @@ namespace SelinaTimeLine.Controls
             var xPerValue = (ActualWidth - (xMargin * 2)) / Maximum;
 
             Console.WriteLine("xPerValue: " + xPerValue);
-
 
             foreach (var eventMarker in EventMarkers)
             {
@@ -52,46 +122,50 @@ namespace SelinaTimeLine.Controls
         private static void DrawEventMarker(DrawingContext dc, int xMargin, double xPosition, int yMargin, EventType eventType)
         {
             //todo: Get the drawing/icon from EventMarker
-            var geo = new PathGeometry
+            //var geo = new PathGeometry
+            //{
+            //    FillRule = FillRule.Nonzero,
+            //    Figures = new PathFigureCollection
+            //    {
+            //        new PathFigure
+            //        {
+            //            // create triangle with (0,0), (6,15), (-6,15) as base
+            //            // after that xPosition just moves those triangle horizontally
+            //            StartPoint = new Point(0 + xMargin + xPosition, 0 + yMargin),
+            //            Segments = new PathSegmentCollection
+            //            {
+            //                new LineSegment
+            //                {
+            //                    Point = new Point(6 + xMargin + xPosition, 15 + yMargin)
+            //                },
+            //                new LineSegment
+            //                {
+            //                    Point = new Point(-6 + xMargin + xPosition, 15 + yMargin)
+            //                },
+            //                new LineSegment
+            //                {
+            //                    Point = new Point(0 + xMargin + xPosition, 0 + yMargin)
+            //                }
+            //            }
+            //        }
+            //    }
+            //};
+
+            //var fillColor = eventType == EventType.Foul ? Colors.Red : Colors.Blue;
+            //var borderColor = eventType == EventType.Foul ? Colors.DarkRed : Colors.DarkBlue;
+
+
+            //dc.DrawGeometry(new SolidColorBrush(fillColor), new Pen(new SolidColorBrush(borderColor), 1), geo);
+
+            var markerDrawing = eventType == EventType.Foul ? FoulEventMarker : GoalEventMarker;
+
+            if (markerDrawing == null)
             {
-                FillRule = FillRule.Nonzero,
-                Figures = new PathFigureCollection
-                {
-                    new PathFigure
-                    {
-                        // create triangle with (0,0), (6,15), (-6,15) as base
-                        // after that xPosition just moves those triangle horizontally
-                        StartPoint = new Point(0 + xMargin + xPosition, 0 + yMargin),
-                        Segments = new PathSegmentCollection
-                        {
-                            new LineSegment
-                            {
-                                Point = new Point(6 + xMargin + xPosition, 15 + yMargin)
-                            },
-                            new LineSegment
-                            {
-                                Point = new Point(-6 + xMargin + xPosition, 15 + yMargin)
-                            },
-                            new LineSegment
-                            {
-                                Point = new Point(0 + xMargin + xPosition, 0 + yMargin)
-                            }
-                        }
-                    }
-                }
-            };
+                return;
+            }
 
-            var fillColor = eventType == EventType.Foul ? Colors.Red : Colors.Blue;
-            var borderColor = eventType == EventType.Foul ? Colors.DarkRed : Colors.DarkBlue;
-
-
-            dc.DrawGeometry(new SolidColorBrush(fillColor), new Pen(new SolidColorBrush(borderColor), 1), geo);
-        }
-
-        public List<EventMarker> EventMarkers
-        {
-            get => (List<EventMarker>) GetValue(EventMarkersProperty);
-            set => SetValue(EventMarkersProperty, value);
+            var rect = new Rect(xPosition, yMargin, markerDrawing.Width, markerDrawing.Height);
+            dc.DrawImage(markerDrawing, rect);
         }
     }
 }
