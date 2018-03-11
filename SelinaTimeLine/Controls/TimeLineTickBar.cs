4c68af4271eb2b17ef4a789938997daa27553326
Shamsad Zaman <shamsad.zaman@selise.ch.bd>
2018-03-11 15:59:39 +0600

    make repeatButtons bigger for easy navigation

--- SelinaTimeLine/Controls/TimeLineTickBar.cs
+++ SelinaTimeLine/Controls/TimeLineTickBar.cs
@@ -11,6 +11,9 @@ namespace SelinaTimeLine.Controls
 {
     public class TimeLineTickBar : TickBar
     {
+        private const int XMargin = 6;
+        private const int YMargin = -26;
+
         public static readonly DependencyProperty EventMarkersProperty = DependencyProperty.Register("EventMarkers", typeof(List<EventMarker>), typeof(TimeLineTickBar), new PropertyMetadata(default(List<EventMarker>)));
         public static readonly DependencyProperty GoalProperty = DependencyProperty.Register("Goal", typeof(Canvas), typeof(TimeLineTickBar), new PropertyMetadata(default(Canvas), GPropertyChangedCallback));
 
@@ -92,12 +95,11 @@ namespace SelinaTimeLine.Controls
                 return;
             }
 
-            var xMargin = 6;
-            var yMargin = -7;
+            
 
             // subtract margin from left/right side to get the width of
             // the slider line
-            var xPerValue = (ActualWidth - (xMargin * 2)) / Maximum;
+            var xPerValue = (ActualWidth - (XMargin * 2)) / Maximum;
 
             Console.WriteLine("xPerValue: " + xPerValue);
 
@@ -111,11 +113,11 @@ namespace SelinaTimeLine.Controls
 
                 var xPosition = xPerValue * eventMarker.FromTime.TotalMilliseconds;
 
-                Console.WriteLine("(ActualWidth - (xMargin * 2)): " + (ActualWidth - (xMargin * 2)));
+                Console.WriteLine("(ActualWidth - (xMargin * 2)): " + (ActualWidth - (XMargin * 2)));
                 Console.WriteLine("eventMarker.FromTime.Milliseconds / Maximum" + eventMarker.FromTime.TotalMilliseconds );
                 Console.WriteLine("xPosition: " + xPosition);
 
-                DrawEventMarker(dc, xMargin, xPosition, yMargin, eventMarker.EventType);
+                DrawEventMarker(dc, XMargin, xPosition, YMargin, eventMarker.EventType);
             }
         }
 
@@ -163,7 +165,7 @@ namespace SelinaTimeLine.Controls
             {
                 return;
             }
-
+            
             var rect = new Rect(xPosition + xMargin, yMargin, markerDrawing.Width, markerDrawing.Height);
             dc.DrawImage(markerDrawing, rect);
         }
